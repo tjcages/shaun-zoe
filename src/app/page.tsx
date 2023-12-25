4ad76daea383257b7d22c75fea4b54aebd04f0e3
tylerj <tylerjcagle@gmail.com>
2023-12-24 22:19:27 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="fixed top-0 left-0 right-0 bottom-0 flex min-h-screen flex-col items-center justify-between p-24">
       <Canvas />
     </main>
   );
